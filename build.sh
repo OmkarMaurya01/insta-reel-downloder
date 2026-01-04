db110e47a5859f798f76d3ca692b560ff22de1dd
Omkar Maurya <omkarmaurya178@gmail.com>
2026-01-04 15:17:28 +0530

    fix one more

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Run migrations (optional but recommended)
+python manage.py collectstatic --no-input
 python manage.py migrate
